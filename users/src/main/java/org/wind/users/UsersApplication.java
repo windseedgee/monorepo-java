27c1018f572adbd0605980fcce55ef2bd37ce118
OriZhai <ori.zhai@cipherowl.com>
2024-05-06 14:51:56 +0800

    test circle ci config

--- users/src/main/java/org/wind/users/UsersApplication.java
+++ users/src/main/java/org/wind/users/UsersApplication.java
@@ -1,13 +1,18 @@
 package org.wind.users;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UsersApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(UsersApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(UsersApplication.class, args);
+        log.debug("test users");
     }
 
 }
