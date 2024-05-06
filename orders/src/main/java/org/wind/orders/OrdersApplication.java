65c97dbe768d6b03dbdbb3f91d9fd0102aeef9b7
windseedgee <55382107+windseedgee@users.noreply.github.com>
2024-05-06 16:05:53 +0800

    test circle ci config (#2)
    modify circleci config

--- orders/src/main/java/org/wind/orders/OrdersApplication.java
+++ orders/src/main/java/org/wind/orders/OrdersApplication.java
@@ -1,13 +1,18 @@
 package org.wind.orders;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrdersApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(OrdersApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(OrdersApplication.class, args);
+        log.debug("test orders");
     }
 
 }
