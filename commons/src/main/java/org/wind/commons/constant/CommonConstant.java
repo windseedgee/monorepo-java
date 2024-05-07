27e4df1d852a916b67bb6a8f23bf5ce7840e0d46
windseedgee <55382107+windseedgee@users.noreply.github.com>
2024-05-07 10:31:46 +0800

    add commons module (#4)
    Co-authored-by: OriZhai <ori.zhai@cipherowl.com>

---
+++ commons/src/main/java/org/wind/commons/constant/CommonConstant.java
@@ -0,0 +1,10 @@
+package org.wind.commons.constant;
+
+public class CommonConstant {
+
+    public static final String USERS_SERVICE_NAME = "users-service";
+
+    public static final String ORDERS_SERVICE_NAME = "orders-service";
+
+    public static final String COMMON_SERVICE_NAME = "commons-service";
+}
