b3ca84f6804b1e0bacf7f326e1d31672cb2e2458
OriZhai <ori.zhai@cipherowl.com>
2024-05-07 10:30:14 +0800

    add commons module

--- users/src/main/java/org/wind/users/UsersApplication.java
+++ users/src/main/java/org/wind/users/UsersApplication.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.wind.commons.constant.CommonConstant;
 
 @SpringBootApplication
 public class UsersApplication {
@@ -12,7 +13,7 @@ public class UsersApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UsersApplication.class, args);
-        log.debug("test users----");
+        log.info("test users----{}", CommonConstant.USERS_SERVICE_NAME);
     }
 
 }
